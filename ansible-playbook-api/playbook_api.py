6ea0d5324eab52f1012823760786d1ecacce00ab
web <liuyangc33@gmail.com>
2016-06-15 18:52:07 +0800

    Update playbook_api.py

--- ansible-playbook-api/playbook_api.py
+++ ansible-playbook-api/playbook_api.py
@@ -11,34 +11,34 @@ from ansible.executor.playbook_executor import PlaybookExecutor
 
 
 Options = namedtuple('Options', [
+    'inventory',
+    'sudo', 'su', 'sudo_user', 'su_user',
     'listtags', 'listtasks', 'listhosts',
     'syntax', 'check',
+    'ask_pass', 'ask_su_pass', 'ask_sudo_pass', 'ask_vault_pass',
     'connection', 'timeout', 'remote_user', 'private_key_file',
     'module_path', 'forks',
     'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args',
     'become', 'become_method', 'become_user',
     'verbosity'])
 
-options = Options(
-    listtags=False, listtasks=False, listhosts=False,
-    syntax=False, check=False,
-
-    # connection
-    connection='ssh', timeout=10, remote_user=None, private_key_file=None,
-
-    # args
-    ssh_common_args=None, ssh_extra_args=None,
-    sftp_extra_args=None, scp_extra_args=None,
-
-    module_path=None, forks=5,
-    become=True, become_method=None, become_user=None,
-    verbosity=None)
-
 
 class PlayBook(object):
     def __init__(self, playbook, inventory="inventory.py", extra_vars=None):
-        self.playbook = "%s/%s" % (os.path.dirname(__file__), playbook)
-        self.options = options
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        self.playbook = os.path.join(basedir, playbook)
+        self.options = Options(
+            inventory=inventory,
+            sudo=False, su=False, sudo_user=None, su_user=None,
+            listtags=False, listtasks=False, listhosts=False,
+            syntax=False, check=False,
+            ask_pass=False,ask_su_pass=False,ask_sudo_pass=False,ask_vault_pass=False,
+            connection='smart', timeout=10, remote_user=None, private_key_file=None,
+            ssh_common_args=None, ssh_extra_args=None,
+            sftp_extra_args=None, scp_extra_args=None,
+            module_path=None, forks=5,
+            become=False, become_method='sudo', become_user=None,
+            verbosity=0)
         self.loader = DataLoader()
         self.variable_manager = VariableManager()
         self.inventory = Inventory(loader=self.loader,
@@ -56,6 +56,7 @@ class PlayBook(object):
         stats = self.pbex._tqm._stats
         self.pbex._tqm.send_callback('human_log')
         return stats
+
 
 if __name__ == "__main__":
     pb = PlayBook("your playbook yaml", inventory="inventory.py", extra_vars=None)
