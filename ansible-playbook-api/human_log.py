a6aeec2baca691c1e233ee4d805b3ec6734ef39f
web <liuyangc33@gmail.com>
2016-07-25 15:34:39 +0800

    typo

--- ansible-playbook-api/human_log.py
+++ ansible-playbook-api/human_log.py
@@ -15,18 +15,18 @@ class CallbackModule(CallbackModule_default):
         pass
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        // do sth
+        # do sth
         super(CallbackModule, self)
     
     def v2_runner_on_ok(self, result):
         if result._result.get('changed', False):
-            // do sth
+            # do sth
             super(CallbackModule, self).v2_runner_on_ok(result)
         else:
             pass
 
     def v2_runner_on_unreachable(self, result):
-        // do sth
+        # do sth
         super(CallbackModule, self).v2_runner_on_unreachable(result)
 
     def v2_runner_on_skipped(self, result):
