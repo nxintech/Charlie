c8a80934e8f3fd264af493835ad61b6af0508c8b
Yang Liu <liuyangc33@gmail.com>
2017-02-19 20:50:08 +0800

    Update consumer.groovy

--- activemq-groovy-client/consumer.groovy
+++ activemq-groovy-client/consumer.groovy
@@ -6,7 +6,7 @@
 
 import javax.jms.Session
 import javax.jms.TextMessage
-
+import org.apache.activemq.ActiveMQConnectionFactory
 
 def urls = "tcp://ip1:61616,tcp://ip2:61616,tcp://ip3:61616"
 def brokerUrl = "failover:($urls)?initialReconnectDelay=1000&maxReconnectAttempts=2"
