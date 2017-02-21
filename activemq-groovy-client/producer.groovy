adec5f4732f23f911c0143b5fb8077580ec5d2ff
Yang Liu <liuyangc33@gmail.com>
2017-02-21 09:22:14 +0800

    Update producer.groovy

--- activemq-groovy-client/producer.groovy
+++ activemq-groovy-client/producer.groovy
@@ -1,7 +1,8 @@
 @Grapes([
         @Grab(group = 'javax.jms', module = 'jms', version = '1.1'),
         @Grab(group = 'org.apache.activemq', module = 'activemq-all', version = '5.11.1'),
-        @Grab(group = 'org.apache.logging.log4j', module = 'log4j', version = '2.8')
+        @Grab(group = 'org.apache.logging.log4j', module = 'log4j-api', version = '2.8'),
+        @Grab(group = 'org.apache.logging.log4j', module = 'log4j-core', version = '2.8')
 ])
 
 import javax.jms.Session
