35e30bd9582ae0eb8b401eb96efb95795003e5e3
liuyangc3 <liuyangc33@gmail.com>
2017-11-28 16:36:08 +0800

    message event is async, so when the second jumpserver request get login cookie, but first message login not finish yet, cookie may incorrect, cause body JSONDecodeError.
    how to fix:
    
    1 we login when class init
    
    2 if status code is 302 we relogin again

--- jumpserver/jumpserver_api.py
+++ jumpserver/jumpserver_api.py
@@ -8,9 +8,10 @@ class JumpServer(object):
         self.username = username
         self.password = password
         self.base = base
+        self._cookie = self.get_cookie()
 
     def get_cookie(self):
-        if not self._cookie:
+        if self._cookie is None:
             url = self.base + "login/"
             data = {
                 "username": self.username,
@@ -19,6 +20,12 @@ class JumpServer(object):
             res = requests.post(url=url, data=data)
             self._cookie = res.cookies
         return self._cookie
+
+    def need_retry(self, code):
+        if code == 302:
+            # we need login again
+            self.get_cookie()
+            return True
 
     def add_resource(self, hostname, username, password,
                      port=22, ip=None, group=None, is_active=1):
@@ -33,19 +40,23 @@ class JumpServer(object):
             "is_active": is_active
         }
         res = requests.post(url=url, data=data, cookies=self.get_cookie())
+        if self.need_retry(res.status_code):
+            res = requests.post(url=url, data=data, cookies=self.get_cookie())
         return res.status_code
 
     def del_resource(self, resource_id):
         url = self.base + "jasset/asset/del/?id={}".format(resource_id)
         res = requests.get(url=url, cookies=self.get_cookie())
+        if self.need_retry(res.status_code):
+            res = requests.get(url=url, cookies=self.get_cookie())
         return res.status_code
 
     def search_resource(self, hostname):
         url = self.base + "jasset/asset/search/?hostname={}".format(hostname)
         res = requests.get(url=url, cookies=self.get_cookie())
 
-        if res.status_code != 200:
-            return None, None
+        if self.need_retry(res.status_code):
+            res = requests.get(url=url, cookies=self.get_cookie())
 
         data = res.json()
         if not data:
@@ -57,6 +68,8 @@ class JumpServer(object):
     def edit_resource(self, resource_id, data):
         url = self.base + "jasset/asset/edit/?id={}".format(resource_id)
         res = requests.post(url=url, data=data, cookies=self.get_cookie())
+        if self.need_retry(res.status_code):
+            res = requests.post(url=url, data=data, cookies=self.get_cookie())
         return res.status_code
 
 
