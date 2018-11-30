9127ef8e324213ef189ea5805fec51d2a300972f
Yang Liu <liuyangc33@gmail.com>
2018-11-30 10:09:00 +0800

    Update encryptpy3.py

--- disconfig/encryptpy3.py
+++ disconfig/encryptpy3.py
@@ -1,24 +1,34 @@
-# install Cryptodome
 import base64
 from Crypto import Random
 from Crypto.Cipher import AES
 
+# install Cryptodome instead of pycrypto
+
 BS = AES.block_size
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s : s[0:-s[-1]]
-
-with open('key', 'rb') as f:
-    key = f.read()
 
 
-def encrypt(raw):
-    raw = pad(raw)
+def pad(s: str) -> str:
+    return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+
+
+def unpad(s: bytes) -> bytes:
+    return s[0:-s[-1]]
+
+
+def read(file):
+    with open(file, 'rb') as f:
+        return f.read()
+
+
+def encrypt(string: str) -> str:
+    byte = pad(string).encode()
     iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
+    cipher = AES.new(read('key'), AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(byte)).decode()
 
-def decrypt(encrypt):
-    enc = base64.b64decode(encrypt)
+
+def decrypt(encrypted: str) -> str:
+    enc = base64.b64decode(encrypted)
     iv = enc[:16]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
+    cipher = AES.new(read('key'), AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:])).decode()
